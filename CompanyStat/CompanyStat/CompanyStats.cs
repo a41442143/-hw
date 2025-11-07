72d22ee3076c8b3c86cac88144df49362db94053
jiawen-CHIU <c112151105@nkust.edu.tw>
2025-11-07 11:10:14 +0800

    你好

---
+++ CompanyStat/CompanyStat/CompanyStats.cs
@@ -0,0 +1,16 @@
+﻿#nullable disable
+using System.Text.Json.Serialization;
+
+public class CompanyStats
+{
+    public string 月別 { get; set; }
+
+    [JsonPropertyName("上市公司-家數")]
+    public string 上市公司_家數 { get; set; }
+
+    [JsonPropertyName("上櫃公司-家數")]
+    public string 上櫃公司_家數 { get; set; }
+
+    [JsonPropertyName("未上市未上櫃公司-家數")]
+    public string 未上市_家數 { get; set; }
+}
---
+++ CompanyStat/CompanyStat/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+
+string filePath = "App_Data/company.json";
+
+if (!File.Exists(filePath))
+{
+    Console.WriteLine("找不到資料檔案，請確認 App_Data/company.json 是否存在！");
+    return;
+}
+
+string json = File.ReadAllText(filePath);
+
+List<CompanyStats>? data = JsonSerializer.Deserialize<List<CompanyStats>>(json);
+
+Console.WriteLine("近月台灣公司統計資料");
+Console.WriteLine("------------------------------------------------------");
+Console.WriteLine("{0,-10} {1,8} {2,8} {3,10}",
+    "月份", "上市家數", "上櫃家數", "未上市家數");
+Console.WriteLine("------------------------------------------------------");
+
+foreach (var item in data)
+{
+    Console.WriteLine("{0,-10} {1,8} {2,8} {3,10}",
+        item.月別,
+        item.上市公司_家數,
+        item.上櫃公司_家數,
+        item.未上市_家數);
+}
+
+Console.WriteLine("\n資料讀取完成！");
+Console.ReadLine();
