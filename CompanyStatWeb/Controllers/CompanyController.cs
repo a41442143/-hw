cb82a04413b015b0a0f4070096f30687d4008842
a41442143 <fb2000327@gmail.com>
2025-12-22 14:51:55 +0800

    build a web

---
+++ CompanyStatWeb/Controllers/CompanyController.cs
@@ -0,0 +1,142 @@
+using Microsoft.AspNetCore.Mvc;
+using CompanyStatWeb.Models;
+using CompanyStatWeb.Services;
+
+namespace CompanyStatWeb.Controllers
+{
+    public class CompanyController : Controller
+    {
+        private readonly CompanyDataService _service;
+
+        public CompanyController(CompanyDataService service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Index(string sortOrder, string searchString)
+        {
+            ViewData["CurrentFilter"] = searchString;
+            ViewData["MonthSortParm"] = String.IsNullOrEmpty(sortOrder) ? "month_desc" : "";
+            ViewData["CountSortParm"] = sortOrder == "Count" ? "count_desc" : "Count";
+            ViewData["CapitalSortParm"] = sortOrder == "Capital" ? "capital_desc" : "Capital";
+            ViewData["MarketValueSortParm"] = sortOrder == "MarketValue" ? "marketValue_desc" : "MarketValue";
+
+            var data = _service.GetAll().AsEnumerable();
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                data = data.Where(s => s.Month.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "month_desc":
+                    data = data.OrderByDescending(s => s.Month);
+                    break;
+                case "Count":
+                    data = data.OrderBy(s => Convert.ToDouble(s.PublicCompanyCount));
+                    break;
+                case "count_desc":
+                    data = data.OrderByDescending(s => Convert.ToDouble(s.PublicCompanyCount));
+                    break;
+                case "Capital":
+                    data = data.OrderBy(s => Convert.ToDouble(s.PublicCompanyCapital));
+                    break;
+                case "capital_desc":
+                    data = data.OrderByDescending(s => Convert.ToDouble(s.PublicCompanyCapital));
+                    break;
+                case "MarketValue":
+                    data = data.OrderBy(s => Convert.ToDouble(s.PublicCompanyMarketValue));
+                    break;
+                case "marketValue_desc":
+                    data = data.OrderByDescending(s => Convert.ToDouble(s.PublicCompanyMarketValue));
+                    break;
+                default:
+                    data = data.OrderBy(s => s.Month);
+                    break;
+            }
+
+            return View(data.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CompanyStats company)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _service.Add(company);
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", ex.Message);
+                }
+            }
+            return View(company);
+        }
+
+        public IActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var company = _service.GetById(id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+            return View(company);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(string id, CompanyStats company)
+        {
+            if (id != company.Month)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _service.Update(company);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(company);
+        }
+
+        public IActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var company = _service.GetById(id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return View(company);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(string id)
+        {
+            _service.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
